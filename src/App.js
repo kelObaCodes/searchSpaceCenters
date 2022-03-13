e776674709bb0f03f6a9847affa8a1933f6db8f7
kelvinOba <kelvinovbiosa@gmail.com>
2022-03-13 16:47:57 +0100

    Make navbar sticky

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-import Map  from './components/Map'
 import AppContainer from "./container/index"
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,33 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { InMemoryCache,ApolloProvider, ApolloClient} from '@apollo/client'
-import store from "./redux/store"
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { InMemoryCache, ApolloProvider, ApolloClient } from "@apollo/client";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 const http = new ApolloClient({
-  uri:process.env.REACT_APP_API_URL,
-cache: new InMemoryCache(),
-headers: {
-  "Authorization": "Bearer API_KEY"
-}
-})
-
+  uri: process.env.REACT_APP_API_URL,
+  cache: new InMemoryCache(),
+  headers: {
+    Authorization: "Bearer API_KEY",
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-      <Provider store={store}>
-
-    <ApolloProvider client={http}>
-    <App />
-
-    </ApolloProvider>
+    <Provider store={store}>
+      <ApolloProvider client={http}>
+        <App />
+      </ApolloProvider>
     </Provider>
-
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
